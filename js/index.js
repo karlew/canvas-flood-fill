3ee8f5cd7534837ce55433d2fb4beb7cd5a1ef5c
karlew <lu@karlew.com>
2017-08-14 16:20:07 +0800

    规范变量名

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function() {
+	var chooseColor, 
+	myCanvas = document.getElementById("canvas"), 
+	ctx = myCanvas.getContext("2d"), 
+	image = new Image();
+	image.onload = function() {
+		ctx.drawImage(image, 0, 0)
+	};
+	image.src = "img/img_canvas.png";
+	$("#wire").on("click", function(e) {
+		if (!chooseColor) {
+			alert("请选择要填充的颜色");
+			return false
+		}
+		e = e.originalEvent || e;
+		var wireOffset = $(this).offset();
+		var left = Math.floor(e.pageX - wireOffset.left);
+		var top = Math.floor(e.pageY - wireOffset.top);
+		var imgData = ctx.getImageData(left, top, 1, 1);
+		var colorArr = imgData.data;
+		if (colorArr[0] === 0 && colorArr[1] === 0 && colorArr[2] === 0 && colorArr[3] === 0) {
+			alert("此处不能上色");
+			return false
+		}
+		var fillColor = hexToRgb(chooseColor).split(",");
+		fillColor.push(255);
+		floodFillLinear(myCanvas, left, top, fillColor, 80);
+	});
+	$("#colors").on("click", "li:not(.checked)", function() {
+		$(this).addClass("checked").siblings(".checked").removeClass("checked");
+		chooseColor = $(this).html()
+	});
+});
